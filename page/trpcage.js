113646d52c597b4699473736440e1a6626af680f
hyun <shpark26@postech.ac.kr>
2024-11-18 03:46:50 +0000

    fix - transition replaced to gifs

--- page/trpcage.js
+++ page/trpcage.js
@@ -1,56 +1,55 @@
 document.addEventListener("DOMContentLoaded", function () {
-  stage_trpcage = new NGL.Stage("viewport-trpcage");
-  stage_trpcage.setParameters({ backgroundColor: "white" });
-  stage_trpcage
-    .loadFile("./page/data/trpcage/trpcage.gro", {
-      defaultRepresentation: false,
-      asTrajectory: true,
-    })
-    .then(function (comp) {
-      comp.setName("simulation-trpcage");
-      // comp.removeRepresentation("ribbon")
-      comp.addRepresentation("ball+stick", {
-        aspectRatio: 1.0,
-        radius: 0.15,
-      });
-      comp.addTrajectory();
-    });
+  // stage_trpcage = new NGL.Stage("viewport-trpcage");
+  // stage_trpcage.setParameters({ backgroundColor: "white" });
+  // stage_trpcage
+  //   .loadFile("./page/data/trpcage/trpcage.gro", {
+  //     defaultRepresentation: false,
+  //     asTrajectory: true,
+  //   })
+  //   .then(function (comp) {
+  //     comp.setName("simulation-trpcage");
+  //     comp.addRepresentation("ball+stick", {
+  //       aspectRatio: 1.0,
+  //       radius: 0.15,
+  //     });
+  //     comp.addTrajectory();
+  //   });
 
-  var toggleSpin_trpcage = document.getElementById(
-    "toggleSpin-trpcage"
-  );
-  var isSpinning_trpcage = false;
-  toggleSpin_trpcage.addEventListener("click", function () {
-    if (!isSpinning_trpcage) {
-      stage_trpcage.setSpin([0, 1, 0], 0.01);
-      isSpinning_trpcage = true;
-    } else {
-      stage_trpcage.setSpin(null, null);
-      isSpinning_trpcage = false;
-    }
-  });
+  // var toggleSpin_trpcage = document.getElementById(
+  //   "toggleSpin-trpcage"
+  // );
+  // var isSpinning_trpcage = false;
+  // toggleSpin_trpcage.addEventListener("click", function () {
+  //   if (!isSpinning_trpcage) {
+  //     stage_trpcage.setSpin([0, 1, 0], 0.01);
+  //     isSpinning_trpcage = true;
+  //   } else {
+  //     stage_trpcage.setSpin(null, null);
+  //     isSpinning_trpcage = false;
+  //   }
+  // });
 
-  var toggleRunMDs_trpcage = document.getElementById(
-    "toggleRunMDs-trpcage"
-  );
-  var isRunning_trpcage = false;
-  toggleRunMDs_trpcage.addEventListener("click", function () {
-    var trajComp_trpcage = stage_trpcage.getComponentsByName(
-      "simulation-trpcage"
-    ).list[0].trajList[0];
-    var player_trpcage = new NGL.TrajectoryPlayer(
-      trajComp_trpcage.trajectory,
-      { step: 80, mode: "once", interpolateType: "spline", timeout: 200 }
-    );
-    if (!isRunning_trpcage) {
-      player_trpcage.play();
-      isRunning_trpcage = true;
-    } else {
-      player_trpcage.play();
-      player_trpcage.pause();
-      isRunning_trpcage = false;
-    }
-  });
+  // var toggleRunMDs_trpcage = document.getElementById(
+  //   "toggleRunMDs-trpcage"
+  // );
+  // var isRunning_trpcage = false;
+  // toggleRunMDs_trpcage.addEventListener("click", function () {
+  //   var trajComp_trpcage = stage_trpcage.getComponentsByName(
+  //     "simulation-trpcage"
+  //   ).list[0].trajList[0];
+  //   var player_trpcage = new NGL.TrajectoryPlayer(
+  //     trajComp_trpcage.trajectory,
+  //     { step: 80, mode: "once", interpolateType: "spline", timeout: 200 }
+  //   );
+  //   if (!isRunning_trpcage) {
+  //     player_trpcage.play();
+  //     isRunning_trpcage = true;
+  //   } else {
+  //     player_trpcage.play();
+  //     player_trpcage.pause();
+  //     isRunning_trpcage = false;
+  //   }
+  // });
 
   stage_trpcage_initial = new NGL.Stage("viewport-trpcage-initial");
   stage_trpcage_initial.setParameters({ backgroundColor: "white" });
